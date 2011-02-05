7b15ac811750f4366d9228d626387f39b77ae908
Damien Krotkine <dams@cpan.org>
2011-02-05 13:06:20 +0100

    removed the persistence option, and simplified the plugin to its bare minimal

--- lib/Dancer/Plugin/FlashMessage.pm
+++ lib/Dancer/Plugin/FlashMessage.pm
@@ -15,13 +15,12 @@ my $conf = plugin_setting;
 
 my $token_name       = $conf->{token_name}       || 'flash';
 my $session_hash_key = $conf->{session_hash_key} || '_flash';
-my $persistence      = $conf->{persistence}      || 0;
 
 register flash => sub ($;$) {
     my ($key, $value) = @_;
     my $flash = session $session_hash_key || {};
     @_ == 2 and $flash->{$key} = $value;
-    @_ == 1 and $value = $persistence ? $flash->{$key} : delete $flash->{$key};
+    @_ == 1 and $value = delete $flash->{$key};
     session $session_hash_key, $flash;
     return $value;
 };
@@ -30,7 +29,7 @@ before_template sub {
    shift->{$token_name} = {  map { my $key = $_; my $value;
                                    ( $key, sub { defined $value and return $value;
                                                  my $flash = session $session_hash_key || {};
-                                                 $value = $persistence ? $flash->{$key} : delete $flash->{$key};
+                                                 $value = delete $flash->{$key};
                                                  session $session_hash_key, $flash;
                                                  return $value;
                                                } );
@@ -98,16 +97,14 @@ change the behaviour of the plugin. See L<CONFIGURATION>
   # sets the flash message for the warning key
   flash warning => 'some warning message';
 
-  # retrieves and removes (unless persistence is true) the flash message for
-  # the warning key
+  # retrieves and removes the flash message for the warning key
   my $warning_message = flash 'warning';
 
 This method can take 1 or 2 parameters. When called with two parameters, it
 sets the flash message for the given key.
 
 When called with one parameter, it returns the value of the flash message of
-the given key. It usually also deletes this entry, but if you have set the
-'persistence' configuration key to true, the entry won't be deleted. See below
+the given key. The message is deleted from the flash hash in the session.
 
 In both cases, C<flash> always returns the value;
 
@@ -122,19 +119,12 @@ These are the default values. See below for a description of the keys
 
   plugins:
     FlashMessage:
-      persistence: 0
       token_name: flash
       session_hash_key: _flash
 
 =head2 configuration description
 
 =over
-
-=item persistence
-
-If set to a true value, flash messages will be persistent, i.e. survive more
-than one request. If set to a false value, flash messages will be suppressed
-once templating has been done. B<Default> : C<0>
 
 =item token_name
 
